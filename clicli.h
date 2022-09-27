cc460ee9e2080dbd3c2252817e5c26ad72594cfa
galarb <84223483+galarb@users.noreply.github.com>
2022-09-27 11:26:06 +0300

    with read functions

--- clicli.h
+++ clicli.h
@@ -1,13 +1,11 @@
 #ifndef CLICLI_H
 #define CLICLI_H
-//namespace utilities {
  class clicli {
-   
- // private:
 
   public:
    clicli();
    void begin();   
+   void run();   
+
  };
-//}
 #endif 
